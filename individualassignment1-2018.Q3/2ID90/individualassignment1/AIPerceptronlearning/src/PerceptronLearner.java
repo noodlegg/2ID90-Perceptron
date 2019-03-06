61633151aa791064e29b956624a77ab29c91e2b5
NoodleGG <TGian98@gmail.com>
2019-03-06 21:48:56 +0100

    Initial commit
    template files extracted

---
+++ individualassignment1-2018.Q3/2ID90/individualassignment1/AIPerceptronlearning/src/PerceptronLearner.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class PerceptronLearner {
+    
+    /**
+     * The method that implements perceptron learning
+     * @return answer string
+     */
+    public String execute(List<PVector> positive, List<PVector> negative, Boolean bias, Integer maxIterations, List<PVector> queries)
+    {
+        //implementation here
+        
+        return "your output";
+    }
+}
